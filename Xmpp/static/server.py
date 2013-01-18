e49686a9c6b7b1e5e2deb2599151d7fd0eeeac9c
wangsong <wangsong1233276@sina.com>
2013-01-18 16:33:50 +0800

    XX

---
+++ Xmpp/static/server.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from flask import Flask,render_template
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    render_template('hello.html')
+
+if __name__ == "__main__":
+    app.run()
